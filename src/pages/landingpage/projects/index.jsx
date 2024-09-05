dd36bb495523fb2fc6b8bdc05931dd8950f44012
harzyz <abdulazeezahmed85@gmail.com>
2024-09-04 20:33:52 -0700

    complete projects

--- src/pages/landingpage/projects/index.jsx
+++ src/pages/landingpage/projects/index.jsx
@@ -5,46 +5,87 @@ import Link from "next/link";
 import Arrow from "/public/images/Vector.svg";
 import Collab from "/public/images/collab.png";
 import Rogue from "/public/images/roguedev.png";
+import Baselink from "/public/images/baselink.png";
+import Digi from "/public/images/digidish.png";
+import Kemmiespecial from "/public/images/kemmispecial.png";
+import Lenbo from "/public/images/lenbo.png";
 import Image from "next/image";
 
 const Projects = () => {
+  const Projects = [
+    {
+      id: 1,
+      label: "Collab",
+      description: "Task assignment",
+      link: "",
+      imgUrl: Collab,
+      label1: "Rogue dev",
+      description1: "Software website",
+      link1: "",
+      imgUrl1: Rogue,
+    },
+    {
+      id: 2,
+      label: "Digi dish",
+      description: "E-menu website",
+      link: "",
+      imgUrl: Digi,
+      label1: "Baselink",
+      description1: "E-learning",
+      link1: "",
+      imgUrl1: Baselink,
+    },
+    {
+      id: 3,
+      label: "Kemmiespecial",
+      description: "Catering service",
+      link: "",
+      imgUrl: Kemmiespecial,
+      label1: "Lenbo",
+      description1: "Loan app",
+      link1: "",
+      imgUrl1: Lenbo,
+    },
+  ];
   return (
     <div className={styles.wrapper}>
       <Header label={"Projects"} />
-      <div className={styles.special_magic}>
-        <div className={styles.special_box}>
-          <div className={styles.special_boxsub}>
-            <Image src={Collab} alt="Collab" />
-          </div>
-          <div className={styles.detail_box}>
-            <div className={styles.detail_wrap}>
-              <h3>Collab</h3>
-              <p>Task assignment</p>
+      {Projects.map((project) => (
+        <div key={project.id} className={styles.special_magic}>
+          <div className={styles.special_box}>
+            <div className={styles.special_boxsub}>
+              <Image src={project.imgUrl} alt={project.label} />
             </div>
-            <Link className={styles.link} href="">
-              <div className={styles.view_more}>
-                View <Image src={Arrow} alt="Arrow" />
-              </div>{" "}
-            </Link>
+            <div className={styles.detail_box}>
+              <div className={styles.detail_wrap}>
+                <h3>{project.label}</h3>
+                <p>{project.description}</p>
+              </div>
+              <Link className={styles.link} href={project.link}>
+                <div className={styles.view_more}>
+                  View <Image src={Arrow} alt="Arrow" />
+                </div>{" "}
+              </Link>
+            </div>
+          </div>
+          <div className={styles.magic_box}>
+            <div className={styles.magic_boxsub}>
+              <Image src={project.imgUrl1} alt={project.label1}/>
+            </div>
+            <div className={styles.detail_box}>
+              <div className={styles.detail_wrap}>
+                <h3>{project.label1}</h3>
+                <p>{project.description1}</p>
+              </div>
+              <Link className={styles.link} href="">
+                <div className={styles.view_more}>
+                  View <Image src={Arrow} alt="Arrow" />
+                </div>{" "}
+              </Link>
+            </div>
           </div>
         </div>
-        <div className={styles.magic_box}>
-          <div className={styles.magic_boxsub}>
-            <Image src={Rogue} alt="Collab" />
-          </div>
-          <div className={styles.detail_box}>
-            <div className={styles.detail_wrap}>
-              <h3>Rogue dev</h3>
-              <p>Software website</p>
-            </div>
-            <Link className={styles.link} href="">
-              <div className={styles.view_more}>
-                View <Image src={Arrow} alt="Arrow" />
-              </div>{" "}
-            </Link>
-          </div>
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
