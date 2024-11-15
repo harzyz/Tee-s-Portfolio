2a9cd87e90827a6143a79a8bfe9565a223401dda
harzyz <abdulazeezahmed85@gmail.com>
2024-11-15 08:58:02 +0100

    resumption update

---
+++ src/components/skills-card/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "./styles.module.scss";
+
+const SkillsCard = ({ skill }) => {
+  const { id, skill_label, desc } = skill;
+  return (
+    <div className={styles.wrapper}>
+      <span className={styles.item_number}>{`O` + id}</span>
+      <span className={styles.item_info}>
+        <p className={styles.item_label}>{skill_label}</p>
+        <p className={styles.item_desc}>{desc}</p>
+      </span>
+    </div>
+  );
+};
+
+export default SkillsCard;
--- src/features/landingpage/skills/index.jsx
+++ src/features/landingpage/skills/index.jsx
@@ -1,13 +1,51 @@
-import React from 'react'
-import styles from './styles.module.css'
-import Header from '@/components/header'
+import React from "react";
+import styles from "./styles.module.scss";
+import Header from "@/components/header";
+import SkillsCard from "@/components/skills-card";
 
 const Skills = () => {
+  const Skills = [
+    {
+      id: 1,
+      skill_label: 'Problem solving',
+      desc: '"Crafting Innovative Solutions for Complex Challenges"',
+    },
+    {
+      id: 2,
+      skill_label: 'Problem solving',
+      desc: '"Crafting Innovative Solutions for Complex Challenges"',
+    },
+    {
+      id: 3,
+      skill_label: 'Problem solving',
+      desc: '"Crafting Innovative Solutions for Complex Challenges"',
+    },
+    {
+      id: 4,
+      skill_label: 'Problem solving',
+      desc: '"Crafting Innovative Solutions for Complex Challenges"',
+    },
+    {
+      id: 5,
+      skill_label: 'Problem solving',
+      desc: '"Crafting Innovative Solutions for Complex Challenges"',
+    },
+    {
+      id: 6,
+      skill_label: 'Problem solving',
+      desc: '"Crafting Innovative Solutions for Complex Challenges"',
+    },
+  ];
   return (
     <div className={styles.wrapper}>
-      <Header label={'Skills'} />
+      <Header label={"Skills"} />
+      <div className={styles.inner_wrapper}>
+        {Skills.map((skill) => (
+          <SkillsCard skill={skill} key={skill.id} />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Skills
+export default Skills;
