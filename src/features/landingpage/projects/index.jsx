1cc775035f6635f36fa171974962a023329d7938
harzyz <abdulazeezahmed85@gmail.com>
2024-12-30 14:50:48 +0100

    update projects

--- src/features/landingpage/projects/index.jsx
+++ src/features/landingpage/projects/index.jsx
@@ -1,3 +1,4 @@
+"use client";
 import Header from "@/components/header";
 import React from "react";
 import styles from "./styles.module.scss";
@@ -10,12 +11,14 @@ import Digi from "/public/images/digidish.png";
 import Kemmiespecial from "/public/images/kemmispecial.png";
 import Lenbo from "/public/images/lenbo.png";
 import Image from "next/image";
-import Chopbet from "/public/images/chopbet.png"
-import Clubarant from "/public/images/clubarant.png"
-import Tosin from "/public/images/Tosin.png"
-import ChopLife from "/public/images/choplife.png"
+import Chopbet from "/public/images/chopbet.png";
+import Clubarant from "/public/images/clubarant.png";
+import Tosin from "/public/images/Tosin.png";
+import ChopLife from "/public/images/choplife.png";
 
 const Projects = () => {
+  const [hoveredId, setHoveredId] = React.useState(null);
+
   const Projects = [
     {
       id: 1,
@@ -23,10 +26,12 @@ const Projects = () => {
       description: "Community",
       link: "https://www.lifefriends.co/",
       imgUrl: LifeFriends,
+      videoUrl: "/videos/lifefriends.mp4",
       label1: "ChopBet",
       description1: "Gaming",
       link1: "https://dev.chopbet.ci/",
       imgUrl1: Chopbet,
+      videoUrl1: "/videos/chopbet.mp4",
     },
     {
       id: 2,
@@ -34,10 +39,12 @@ const Projects = () => {
       description: "E-menu website",
       link: "https://www.clubarant.com/",
       imgUrl: Clubarant,
+      videoUrl: "/videos/clubarant.mp4",
       label1: "Tosin Yusuf",
       description1: "Portfolio",
       link1: "https://portfolio-mu-ecru-22.vercel.app/",
       imgUrl1: Tosin,
+      videoUrl1: "/videos/tosin.mp4",
     },
     {
       id: 3,
@@ -45,10 +52,12 @@ const Projects = () => {
       description: "Software website",
       link: "https://www.roguedevtech.com/",
       imgUrl: Rogue,
+      videoUrl: "/videos/rogue.mp4",
       label1: "Choplife Gaming",
       description1: "Gaming",
       link1: "https://www.choplifegaming.com/",
       imgUrl1: ChopLife,
+      videoUrl1: "/videos/choplife.mp4",
     },
   ];
   return (
@@ -57,35 +66,41 @@ const Projects = () => {
       {Projects.map((project) => (
         <div key={project.id} className={styles.special_magic}>
           <div data-aos="zoom-out" className={styles.special_box}>
-            <div className={styles.special_boxsub}>
-              <Image src={project.imgUrl} alt={project.label} />
-            </div>
-            <div className={styles.detail_box}>
-              <div className={styles.detail_wrap}>
-                <h3>{project.label}</h3>
-                <p>{project.description}</p>
-              </div>
-              <Link target="_blank" className={styles.link} href={project.link}>
-                <div className={styles.view_more}>
-                  View <Image src={Arrow} alt="Arrow" />
-                </div>{" "}
-              </Link>
+            <div
+              className={styles.special_boxsub}
+              onMouseEnter={() => setHoveredId(project.label)}
+              onMouseLeave={() => setHoveredId(null)}
+            >
+              {hoveredId === project.label ? (
+                <video
+                  src={project.videoUrl}
+                  autoPlay
+                  loop
+                  muted
+                  className={styles.video}
+                />
+              ) : (
+                <Image src={project.imgUrl} alt={project.label} />
+              )}
             </div>
           </div>
           <div data-aos="zoom-in" className={styles.magic_box}>
-            <div className={styles.magic_boxsub}>
-              <Image src={project.imgUrl1} alt={project.label1}/>
-            </div>
-            <div className={styles.detail_box}>
-              <div className={styles.detail_wrap}>
-                <h3>{project.label1}</h3>
-                <p>{project.description1}</p>
-              </div>
-              <Link target="_blank" className={styles.link} href={project.link1}>
-                <div className={styles.view_more}>
-                  View <Image src={Arrow} alt="Arrow" />
-                </div>{" "}
-              </Link>
+            <div
+              className={styles.magic_boxsub}
+              onMouseEnter={() => setHoveredId(project.label1)}
+              onMouseLeave={() => setHoveredId(null)}
+            >
+              {hoveredId === project.label1 ? (
+                <video
+                  src={project.videoUrl1}
+                  autoPlay
+                  loop
+                  muted
+                  className={styles.video}
+                />
+              ) : (
+                <Image src={project.imgUrl1} alt={project.label} />
+              )}
             </div>
           </div>
         </div>
